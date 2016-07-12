658dafd6caea322bfd41ed69bc8e27215ae37315
Mark van de Veerdonk <mark.vandeveerdonk@luxexcel.com>
2016-07-12 13:14:32 +0200

    Added .NET wrapper DLL and test application.

---
+++ win/TestLibwincorkNET/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WincorkNET;
+
+namespace TestLibwincorkNET
+{
+    class Program
+    {
+        const int ID1 = 1;
+        const int ID2 = 2;
+        const int Out1 = 3;
+        const int Out2 = 4;
+
+        static void Assert(bool f)
+        {
+            if (!f) {
+                Console.Error.WriteLine("Fail!");
+                Environment.Exit(0);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (!Cork.LoadMesh(ID1, "..\\..\\..\\..\\samples\\ballA.off")) {
+                Console.Error.WriteLine("Failed to load file 1");
+                return;
+            }
+
+            if (!Cork.LoadMesh(ID2, "..\\..\\..\\..\\samples\\ballB.off")) {
+                Console.Error.WriteLine("Failed to load file 2");
+                return;
+            }
+
+            Assert(Cork.Union(ID1, ID2, Out1));
+            Assert(Cork.SaveMesh(Out1, "Union.stl"));
+            Assert(Cork.DeleteMesh(Out1));
+            Console.WriteLine("Union done");
+
+            Assert(Cork.Intersection(ID1, ID2, Out1));
+            Assert(Cork.SaveMesh(Out1, "Intersection.stl"));
+            Assert(Cork.DeleteMesh(Out1));
+            Console.WriteLine("Intersection done");
+
+            Assert(Cork.Difference(ID1, ID2, Out1));
+            Assert(Cork.SaveMesh(Out1, "Difference.stl"));
+            Assert(Cork.DeleteMesh(Out1));
+            Console.WriteLine("Difference done");
+
+            Assert(Cork.Xor(ID1, ID2, Out1));
+            Assert(Cork.SaveMesh(Out1, "Xor.stl"));
+            Console.WriteLine("Xor done");
+
+            Assert(Cork.TranslateZ(Out1, 5.0f));
+            Assert(Cork.SaveMesh(Out1, "difference_moved.stl"));
+            Console.WriteLine("Move done");
+
+            Assert(Cork.CopyMesh(Out1, Out2));
+
+            Assert(Cork.Rotate180X(Out1));
+            Assert(Cork.SaveMesh(Out1, "Difference_moved_and_rotatedX.stl"));
+            Console.WriteLine("RotateX done");
+
+            Assert(Cork.Rotate180Y(Out2));
+            Assert(Cork.SaveMesh(Out2, "difference_moved_and_rotatedY.stl"));
+            Console.WriteLine("RotateY done");
+        }
+    }
+}
---
+++ win/WincorkNET/Cork.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WincorkNET
+{
+    public class Cork
+    {
+        /* Management functions */
+
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern void ClearAllMeshes();
+
+        // Deletes mesh with given ID from memory
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool DeleteMesh(int ID);
+
+        // Creates a copy of mesh identified by srcID and store under destID
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool CopyMesh(int srcID, int destID);
+
+        // Loads the mesh from the file specified by fileName and stores it internally under ID
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool LoadMesh(int ID, string fileName);
+
+        // Save the mesh stored internally under ID as file fileName.
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool SaveMesh(int ID, string fileName);
+
+        /* Cork native functions */
+
+        // Can cork handle the model?
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool IsSolid(int ID);
+
+        // Create union model from models stored internally and identified by InID1 and InID2 
+        // and store as OutID.
+        [DllImport("Wincork.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool Union(int ID1, int ID2, int OutID);
+
+        // Create difference model from model stored internally and identified by InID1 and InID2 
+        // and store as OutID. Model ID2 is subtraced from ID1.
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool Difference(int ID1, int ID2, int OutID);
+
+        // Create intersection model from models stored internally and identified by InID1 and InID2 
+        // and store as OutID.
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool Intersection(int ID1, int ID2, int OutID);
+
+        // Perform XOR operation on models stored internally and identified by InID1 and InID2 
+        // and store as OutID.
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool Xor(int ID1, int ID2, int OutID);
+
+        /* Added manipulation functions */
+
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool TranslateZ(int ID, float deltaZ);
+
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool Rotate180X(int ID);
+
+        [DllImport("Wincork.dll", CallingConvention=CallingConvention.Cdecl)]
+        public static extern bool Rotate180Y(int ID);
+    }
+}
